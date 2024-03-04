32d03470c8fcda00af581441f971947ab5c4384b
Amir Hakim <207092@student.upm.edu.my>
2024-03-04 17:35:16 +0800

    add nodejs exercise

---
+++ calcSum.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+let sum = 0
+let combined = 0
+
+module.exports.log = function(msg) {
+path = msg;
+const { open } = require('node:fs/promises');
+
+(async () => {
+  const file = await open('./inputFile/magic_number.txt');
+
+  for await (const line of file.readLines()) {
+    //console.log(`\n${line}`)
+    count = 0
+    digit = []
+    //console.log('\nnumbers in line :-')
+    for(let i = 0; i < line.length; i++) {
+        let single = line.charAt(i)
+        if(single >= '0' && single <= '9') {
+            digit[count] = single
+            //console.log(digit[count])
+            count++
+        }
+    }
+    combined = digit[0] + digit[digit.length-1]
+    //console.log(`the number in this line is ${combined}`)
+    sum = sum + Number(combined)
+    //console.log(`The total sum for all lines is ${sum}`)
+    //console.log('-------------------------------------')
+  }
+  console.log(sum)
+
+})();
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+
+// module.exports = {dir}
+// const output = require('/calcSum');
+// console.log(`Output: ${require('./calcSum')}`)
+
+var msg = require('./calcSum');
+
+msg.log('./inputFile/magic_number.txt')
