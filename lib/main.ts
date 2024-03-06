1d7a1c5ea32aab16ec94fbcb4ae876e288cf9800
Amir Hakim <207092@student.upm.edu.my>
2024-03-06 14:07:19 +0800

    update ts ex

--- lib/main.ts
+++ lib/main.ts
@@ -1,11 +1,9 @@
 import fs from 'fs';
 export const pathToDir = './inputFile'
 export const path = fs.readdirSync(pathToDir);
-// import {run} from './calcSumTsc'
-import {readfile} from './readfile'
+import { readfile } from './readfile'
 import { compNum } from './compNum';
 import { sumNum } from './sumNum';
-// run()
 
 async function run() {
     const lineCont = await readfile("magic_num_3.txt")
