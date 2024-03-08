8c21c20c9db9742b655cdce2c682d2bea741cf36
Amir Hakim <207092@student.upm.edu.my>
2024-03-08 17:19:52 +0800

    fix readfile test

--- lib/readfile.ts
+++ lib/readfile.ts
@@ -1,6 +1,8 @@
 import { readFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 
+process.env.NODE_ENV = 'utf-8';
+
 export async function readfile(filename: string) {
   let contLine = []
   const filePath = resolve('./inputFile', filename)
