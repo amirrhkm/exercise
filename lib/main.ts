d82971be0d53fbe4049be81d5cc87bcc853d9136
Amir Hakim <207092@student.upm.edu.my>
2024-03-08 16:37:36 +0800

    fix readfile test

--- lib/main.ts
+++ lib/main.ts
@@ -4,12 +4,12 @@ import { sumNum } from './sumNum'
 
 async function run() {
   const lineCont = await readfile('magic_num_3.txt')
-  // const lineNum = compNum(lineCont)
-  // const sum = sumNum(lineNum)
+  const lineNum = compNum(lineCont)
+  const sum = sumNum(lineNum)
   
   console.log(lineCont)
-  // console.log(lineNum)
-  // console.log(sum)
+  console.log(lineNum)
+  console.log(sum)
 }
 
 run()
--- test/readfile.test.ts
+++ test/readfile.test.ts
@@ -1,13 +1,10 @@
 import { describe, expect } from '@jest/globals'
 import { readfile } from '../lib/readfile'
 
-const mockans = ['abc', 'def', 'hij', 'klm']
+const mockans = ['abc', 'def', 'ghi', 'jkl']
 const mockfile = './testnum.txt'
 describe('readfile function test', () => {
   test('test', async () => {
-    //expect(await readfile(mockfile)).toStrictEqual(mockans)
-    const fileCont = await readfile(mockfile)
-    console.log(fileCont)
-    expect(fileCont).toStrictEqual(mockans)
+    expect(await readfile(mockfile)).toStrictEqual(mockans)
   })
 })
