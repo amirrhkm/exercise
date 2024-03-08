9518d075dae65a9c11af7ffcc9d222388ab92fcd
Amir Hakim <207092@student.upm.edu.my>
2024-03-08 15:39:39 +0800

    fix readfile function

--- lib/main.ts
+++ lib/main.ts
@@ -6,7 +6,7 @@ async function run() {
   const lineCont = await readfile('magic_num_3.txt')
   const lineNum = compNum(lineCont)
   const sum = sumNum(lineNum)
-
+  
   console.log(lineCont)
   console.log(lineNum)
   console.log(sum)
--- lib/readfile.ts
+++ lib/readfile.ts
@@ -2,11 +2,9 @@ import { readFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 
 export async function readfile(filename: string) {
-  let contLine: string[] = []
+  let contLine = []
   const filePath = resolve('./inputFile', filename)
   const cont = await readFile(filePath, 'utf8')
-  cont.split(/\r\n/).forEach(line => {
-    contLine.push(line)
-  })
-  return contLine
+  const line = cont.split('\r\n')
+  return line
 }
