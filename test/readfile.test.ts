a5c5ab25cc71cb48401bddcbbb0f4a4e13c66efd
Amir Hakim <207092@student.upm.edu.my>
2024-03-08 09:42:48 +0800

    fix readfile test

--- test/readfile.test.ts
+++ test/readfile.test.ts
@@ -2,7 +2,7 @@ import { describe, expect } from '@jest/globals'
 import { readfile } from '../lib/readfile'
 
 const mockans = ['abc', 'def', 'hij', 'klm']
-const mockfile = '../inputFile/testnum.txt'
+const mockfile = './testnum.txt'
 describe('readfile function test', () => {
   test('test', async () => {
     expect(await readfile(mockfile)).toStrictEqual(mockans)
